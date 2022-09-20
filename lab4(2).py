ca97c8ccd001e245416ca7d69991026153145eff
Pavlo Ieremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2022-09-20 09:29:12 +0300

    Delete lab4(2).py

--- lab4(2).py
+++
@@ -1,6 +0,0 @@
-import math 
-x=int(input("значення х"))
-y=int(input("значення y"))
-z=int(input("значення z"))
-r=float(12*(math.pow(x,2)+ math.sin(y))+ math.sqrt(math.pow(z, 2)+1)/math.log(abs(abs(z)+0.07),3))
-print(r)
